928a7935a66f2977723fc25a477e9040d09f960e
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-04-19 00:52:27 +0200

    modifying base templates

--- Together/main/views.py
+++ Together/main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room, Message, Topic
 
 
 def home(request):
-    return render(request, 'main/home.html')
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+    return render(request, 'main/home.html', context)
+
+def room(request):
+    return

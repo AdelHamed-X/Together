4c4fe041ff1d066ffdbab094b00b63c3ab66cc61
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-04-18 21:22:52 +0200

    Adding Initial Files with some models

---
+++ Together/main/models.py
@@ -0,0 +1,36 @@
+""" Main App models """
+from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+         return self.name
+
+
+class Room(models.Model):
+    """ Room Model that represents the rooms table """
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Message(models.Model):
+    """ Message Model that represents the messages table """
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+            return self.body[0:50]
+    

439c7fd4e555171bddcca6ff9b27b7a918ba5b3f
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-04-19 14:39:24 +0200

    Adding create_room api

--- Together/main/views.py
+++ Together/main/views.py
@@ -6,7 +6,15 @@ from .models import Room, Message, Topic
 def home(request):
     rooms = Room.objects.all()
     context = {"rooms": rooms}
+    
     return render(request, 'main/home.html', context)
 
-def room(request):
-    return
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+
+    return render(request, 'main/room.html', context)
+
+def create_room(request):
+    context = {}
+    return render(request, 'main/room_form.html', context)

d05419e5e1424c9a059202d0ad2b71e33586bc51
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-04-20 05:04:44 +0200

    Added CRUD for room

--- Together/main/urls.py
+++ Together/main/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('room/<str:pk>', views.room, name='room'),
 
     path('create-room/', views.create_room, name='create_room'),
+    path('update-room/<str:pk>', views.update_room, name='update_room'),
+    path('delete_room/<str:pk>', views.delete_room, name='delete_room'),
 ]

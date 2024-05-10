bb229c41c3bfab0111aa571246b01d0932f9d7b0
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-05-10 22:18:06 +0300

    Integrating MySQL

--- Together/settings.py
+++ Together/settings.py
@@ -112,10 +112,21 @@ WSGI_APPLICATION = 'Together.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'together',
+        'USER': os.environ.get('DB_USERNAME'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
